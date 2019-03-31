e6d37434903c3ac8c9fe06e841364aaae9040693
pangardi <bruno_pangardi@hotmail.com>
2019-03-31 12:22:53 -0300

    teste

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -6,9 +6,9 @@ import { LoginComponent } from './login/login.component';
 import { EtapasCadastroComponent } from './etapas-cadastro/etapas-cadastro.component';
 
 let rotas = [
-  {path: "dashboard", component: DashboardComponent},
-  {path: "aaa", component: AnuncioComponent},
-  {path: "login", component: LoginComponent},
+  // {path: "dashboard", component: DashboardComponent},
+  // {path: "teste", component: AnuncioComponent},
+  // {path: "login", component: LoginComponent},
   {path: "conta/nova", component: EtapasCadastroComponent},
   {path: "**", redirectTo: "/login"}
 ]
